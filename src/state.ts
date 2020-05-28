e51fc69151238f6197bece771188b97a6defb145
Thibaud REY <contact@thibaudrey.pro>
2020-05-28 22:48:24 +0200

    Set initial state

---
+++ src/state.ts
@@ -0,0 +1,29 @@
+import uid                      from 'uid-safe';
+import { ReactSimplePeerModel } from 'Models/react-simple-peer.model';
+import SimplePeer               from 'simple-peer';
+import { Stack }                from 'typed-structures';
+import { JoinRequest }          from 'Models/JoinRequest';
+import { ClientOffer }          from 'Models/ClientOffer';
+
+export const initState = {
+    id: uid.sync(15),
+    peers: {},
+    // Room assignment left to createRoom implementation
+    // Since users may want to base it on something business related
+    room: null,
+    signalData: {},
+    connections: new Map<string, {model: ReactSimplePeerModel, peers: []}>(),
+    peerConnection: new SimplePeer(),
+    commands: new Map<string, Function>(),
+    model: {connection: new SimplePeer(), stream: null},
+    joinRequests: new Stack<JoinRequest>(),
+    clientOffers: new Stack<[ClientOffer, string, boolean, string]>(),
+    emitJoinRequest: () => {
+    },
+    emitOfferResponse: () => {
+    },
+    emitInitiatorOffers: () => {
+    },
+    emitJoinAck: () => {
+    }
+};
