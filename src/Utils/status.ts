9616c62d8c60dbed23c06af2aec25374b381ba22
Thibaud REY <contact@thibaudrey.pro>
2020-05-28 22:34:46 +0200

    Plain english for status

---
+++ src/Utils/status.ts
@@ -0,0 +1,8 @@
+export const ReactSimplePeerStatus: {[key: string]: string} = {
+    IDLE:  'Waiting for peers',
+    EMITTING_OFFER: 'Emitting offer',
+    REQUESTING_ACCESS: 'Requesting access',
+    RECEIVING_RESPONSE_ACCESS: 'Receiving response access',
+    CONNECTING_TO_EXISTING_PEERS: 'Connecting to existing peers',
+    JOINING_SESSION: 'Joining session'
+};
