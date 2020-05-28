d72484033021ea494ecc2bea355d40477d6bcbe5
Thibaud REY <contact@thibaudrey.pro>
2020-05-28 22:31:40 +0200

    retry invocation command

---
+++ src/Commands/tellHimToRetry.ts
@@ -0,0 +1,10 @@
+import {IReactSimplePeerState} from '../';
+import { send }                from './Tools/send';
+
+export async function tellHimToRetry(_: string, data: string, state: IReactSimplePeerState) {
+    let target = state.connections.get(data);
+
+    if (target) {
+        await send(target.model.connection, 'RETRY_______________')
+    }
+}
